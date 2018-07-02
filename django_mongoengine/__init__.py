b5aced8c380b6462b6f9d5b0a598d39e166ca278
Glauco <glauco@uriland.it>
2018-07-02 15:12:41 +0200

    * added a monkey patch for backcompatibility fields.

--- django_mongoengine/__init__.py
+++ django_mongoengine/__init__.py
@@ -1,6 +1,9 @@
 from .document import Document, DynamicDocument, EmbeddedDocument, DynamicEmbeddedDocument
 from .queryset import QuerySet, QuerySetNoCache
 
+
+from . import monkeypatch
+
 __all__ = ["QuerySet", "QuerySetNoCache", "Document", "DynamicDocument", "EmbeddedDocument", "DynamicEmbeddedDocument"]
 
 default_app_config = 'django_mongoengine.apps.DjangoMongoEngineConfig'
---
+++ django_mongoengine/monkeypatch.py
@@ -0,0 +1,5 @@
+
+from mongoengine import common
+field = common._import_class("ObjectIdField")
+setattr(field, "verbose_name", None)
+setattr(field, "help_text", None)
